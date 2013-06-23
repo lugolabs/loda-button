ba6df1e1ad31c0de5b03e8ed9dd080cabfd8dd11
Arti Sinani <artisinani@gmail.com>
2013-06-23 16:05:26 +0100

    cleanup js

--- loda-button.js
+++ loda-button.js
@@ -2,30 +2,33 @@
 
 		var methods = {
 			start: function () {
-				var loda = this.data('loda-button-data');
+				var loda = getData(this);
 				if (loda) loda.start();
 			},
 
 			stop: function () {
-				var loda = this.data('loda-button-data');
+				var loda = getData(this);
 				if (loda) loda.stop();
 			}
+		},
+
+		getData = function(el) {
+			return el.data('loda-button-data');
+		},
+
+		setData = function(el, data) {
+			el.data('loda-button-data', data);
 		},
 
 		LodaButton = function(element) {
 			this._element  = element;
 			this._jelement = $(element);
-			if (this._jelement.data('loda-button-data')) return;
-
-			this._jelement.data('loda-button-data', this);
-			this._init();
+			
+			if (getData(this._jelement)) return;
+			setData(this._jelement, this);
 		};
 
 	LodaButton.prototype = {
-		_init: function() {
-
-		},
-
 		start: function() {
 			this._jelement.addClass('loda-btn-loading');
 		},
@@ -40,15 +43,14 @@
 
 		if (methods[options]) {
 			return methods[options].apply(this, Array.prototype.slice.call(arguments, 1));
-		} else if (typeof methods === 'object' || !methods) {
+		} else if (typeof options === 'object' || !options) {
 			return this.each(function() {
 				new LodaButton(this);
 				return this;
 			});
 		} else {
-			$.error( 'Method ' +  method + ' does not exist on lodaButton' );
+			$.error( 'Method ' +  options + ' does not exist on lodaButton' );
 		} 
-
 			
 	};
 
